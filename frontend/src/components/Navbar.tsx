cce173fc57e32807a845d061ac38885daa80cf71
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2023-10-03 17:13:52 +0530

    ui: asset & nft card

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
   const { address, isConnecting, isDisconnected } = useAccount();
 
   return (
-    <div className=" fixedtop-0 bg-[#fed385 bg-[#fcfcff bg-opacity- backdrop-blur-md z-50 w-full flex items-center justify-between px-16 pt-8 pb-3">
+    <div className=" fixedtop-0  bg-[#fed385 bg-[#fcfcff bg-opacity- backdrop-blur-md z-50 w-full flex items-center justify-between px-16 pt-8 pb-3">
       <Link href={'/'} className=" cursor-pointer text-3xl font-semibold">SecureID</Link>
       <div className=" flex items-center justify-between gap-x-6">
         {!isDisconnected ? (
